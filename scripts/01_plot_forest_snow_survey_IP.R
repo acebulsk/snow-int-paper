89a845f8b4dc1f83a64a9ae01a3caa3e5bf7e728
Alex Cebulski <alexcebulski@gmail.com>
2024-01-05 16:51:49 -0700

    add boxplot to add to met fig

--- scripts/01_plot_forest_snow_survey_IP.R
+++ scripts/01_plot_forest_snow_survey_IP.R
@@ -3,6 +3,7 @@
 # bring in state of ablation data when finished analysis and poster is created so we have the saved state of data for reproducability
 # file.copy('../../analysis/interception/data', 'data_archives/interception', recursive = T, overwrite = T, copy.date = T)
 
+ip_filter <- 1
 vza_select <- 45 # vertical zeneith angle selected based on best angle that had alignment between canopy coverage and LAI and IP
 calm_colour <- '#0d0887'
 windy_colour <- '#f89540'
@@ -51,14 +52,18 @@ cold_events <- c(
 
 fsd_all_slim <- fsd_all |>
   filter(!storm_id %in% outlier_events_fsd,
-         IP < 0.9)
+         IP <= ip_filter)
+
+fsd_all_slim_low_wind <- fsd_all |>
+  filter(!storm_id %in% c(outlier_events_fsd, windy_events_fsd),
+         IP <= ip_filter)
 
 fsd_all_met <- left_join(fsd_all_slim, fsd_storm_summary_met |> rename(avg_IP = IP), by = 'storm_id')
-lai <- read.csv('../../analysis/interception/data/lai/results/2023_compiled_lai_vza_15_60.csv')  |>
+lai_measurements <- read.csv('../../analysis/interception/data/lai/results/2023_compiled_lai_vza_15_60.csv')  |>
   select(-X)
-lai <- read.csv('../../analysis/interception/data/lai/lai_site_id_2023_05_04.csv') |>
+lai_df <- read.csv('../../analysis/interception/data/lai/lai_site_id_2023_05_04.csv') |>
   mutate(id = paste0('DSCN6', img_id_short, '.JPG')) |>
-  left_join(lai, by = 'id') |>
+  left_join(lai_measurements, by = 'id', relationship = "many-to-many") |>
   rename(num = old_num) |>
   mutate(vza_label = paste('Max Zenith Angle (deg.):' ,vza))
 
@@ -86,28 +91,7 @@ calm_max_median_wind <- fsd_avg_wind$max_med_wind[fsd_avg_wind$wind_class == cal
 windy_name <- paste('High:', windy_min_median_wind, '–', windy_max_median_wind)
 calm_name <- paste('Low:', calm_min_median_wind, '–', calm_max_median_wind)
 
-fsd_avg_temp <- fsd_storm_summary_met |>
-  filter(!storm_id %in% outlier_events_fsd)|>
-  mutate(temp_class = case_when(
-    storm_id %in% warm_events ~ 'Warm',
-    storm_id %in% cold_events ~ 'Cold',
-    TRUE ~ NA
-  )) |>
-  group_by(temp_class) |>
-  dplyr::summarise(mean_temp = mean(`Air Temp. (°C)`),
-                   min_air_temp = min(`Air Temp. (°C)`),
-                   max_air_temp = max(`Air Temp. (°C)`)) |>
-  filter(is.na(temp_class) == F)
-
-warm_min_temp <- fsd_avg_temp$min_air_temp[fsd_avg_temp$temp_class == 'Warm']
-warm_max_temp <- fsd_avg_temp$max_air_temp[fsd_avg_temp$temp_class == 'Warm']
-cold_min_temp <- fsd_avg_temp$min_air_temp[fsd_avg_temp$temp_class == 'Cold']
-cold_max_temp <- fsd_avg_temp$max_air_temp[fsd_avg_temp$temp_class == 'Cold']
-
-warm_name <- paste('Warm:', warm_min_temp, '–', warm_max_temp, '(°C)')
-cold_name <- paste('Cold:', cold_min_temp, '–', cold_max_temp, '(°C)')
-
-fsd_ip_lai <- left_join(fsd_all_slim, lai, by = c('transect', 'num', 'canopy')) |>
+fsd_ip_lai <- left_join(fsd_all_slim, lai_df, by = c('transect', 'num', 'canopy')) |>
   # select(id, transect, num, canopy, cc, Le, IP, vza) |>
   filter(is.na(cc) == F) |>
   mutate(wind_class = case_when(
@@ -127,7 +111,7 @@ cc <- fsd_ip_lai |>
   filter(vza == vza_select) |>
   ggplot(aes(cc, IP, colour = wind_class)) +
   geom_point() +
-  geom_smooth(method = 'lm', se = F) +
+  # geom_smooth(method = 'lm', se = F) +
   stat_cor(aes(colour = wind_class,label = after_stat(rr.label)), geom = "label", show.legend = F) +
   ylab(ip_y_ax_lab) +
   xlab('Canopy Coverage (-)')  +
@@ -139,7 +123,7 @@ lai <- fsd_ip_lai |>
   filter(vza == vza_select) |>
   ggplot(aes(Le, IP, colour = wind_class)) +
   geom_point() +
-  geom_smooth(method = 'lm', se = F) +
+  # geom_smooth(method = 'lm', se = F) +
   stat_cor(aes(colour = wind_class,label = after_stat(rr.label)), geom = "label", show.legend = F) +
   ylab(ip_y_ax_lab) +
   xlab('Leaf Area Index (-)')  +
@@ -151,3 +135,83 @@ lai <- fsd_ip_lai |>
 cowplot::plot_grid(cc, lai, labels = c('A', 'B'), rel_widths = c(.65, 1))
 
 ggsave('figs/interception/forest_ip.png', device = png, width = 9.5, height = 4)
+
+# LAI boxplot like the airtemp and wind speed plots
+
+fsd_all_slim_low_wind_lai <-
+  left_join(fsd_all_slim_low_wind, lai_df, by = c('transect', 'num', 'canopy')) |>
+  # select(id, transect, num, canopy, cc, Le, IP, vza) |>
+  filter(is.na(cc) == F,
+         vza == vza_select)
+
+min <- round(min(fsd_all_slim_low_wind_lai$Le, na.rm = T), 3)
+max <- round(max(fsd_all_slim_low_wind_lai$Le, na.rm = T), 2)
+step <- round((max-min) / 10, 1)
+
+breaks <- seq(
+  min,
+  max + step,
+  step)
+
+labs_seq <- label_bin_fn(bins = breaks)
+
+stopifnot(tail(breaks, 1) > max)
+stopifnot(length(labs_seq) + 1 == length(breaks))
+
+fsd_all_slim_low_wind_lai$le_binned <- cut(fsd_all_slim_low_wind_lai[,'Le', drop = TRUE], breaks, include.lowest = T)
+
+fsd_all_slim_low_wind_lai$le_labs <- as.numeric(cut(fsd_all_slim_low_wind_lai[,'Le', drop = TRUE],
+                          breaks,
+                          labels = labs_seq,
+                          include.lowest = T
+))
+
+lai_ip_boxplot <- fsd_all_slim_low_wind_lai |>
+  ggplot(aes(le_labs, IP, group = le_labs)) +
+  geom_boxplot()+
+  ylab(ip_y_ax_lab) +
+  xlab('LAI bin (-)')+
+  theme(plot.margin = margin(0.5, 0.5, 0.5, .75, "cm"))
+
+lai_ip_boxplot
+
+fsd_all_slim_low_wind_lai |>
+  ggplot(aes(Le, IP)) +
+  geom_point()+
+  xlab('LAI (-)')
+
+
+# CC boxplot like the airtemp and wind speed plots
+
+min <- round(min(fsd_all_slim_low_wind_lai$cc, na.rm = T), 3)
+max <- round(max(fsd_all_slim_low_wind_lai$cc, na.rm = T), 3)
+step <- round((max-min) / 10, 1)
+
+breaks <- seq(
+  min,
+  max + step,
+  step)
+
+labs_seq <- label_bin_fn(bins = breaks)
+
+stopifnot(tail(breaks, 1) > max)
+stopifnot(length(labs_seq) + 1 == length(breaks))
+
+fsd_all_slim_low_wind_lai$cc_binned <- cut(fsd_all_slim_low_wind_lai[,'cc', drop = TRUE], breaks, include.lowest = T)
+
+fsd_all_slim_low_wind_lai$cc_labs <- as.numeric(cut(fsd_all_slim_low_wind_lai[,'cc', drop = TRUE],
+                             breaks,
+                             labels = labs_seq,
+                             include.lowest = T
+))
+
+fsd_all_slim_low_wind_lai |>
+  ggplot(aes(cc_labs, IP, group = cc_labs)) +
+  geom_boxplot()+
+  xlab('Canopy Coverage (-)')
+
+fsd_all_slim_low_wind_lai |>
+  ggplot(aes(cc, IP)) +
+  geom_point()+
+
+  xlab('Canopy Coverage (-)')
