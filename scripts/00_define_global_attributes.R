274320f9a7cac5e6a5123741d43d0670b11327c2
Alex Cebulski <alexcebulski@gmail.com>
2024-01-05 16:50:52 -0700

    changed default colours added new functions

--- scripts/00_define_global_attributes.R
+++ scripts/00_define_global_attributes.R
@@ -6,7 +6,7 @@ library(cowplot)
 library(plotly)
 
 # theme_bw(base_size = 14)
-options(ggplot2.discrete.colour= c("black", "#56B4E9", "#E69F00"))
+options(ggplot2.discrete.colour= palette.colors(palette = "R4"))
 
 theory_colours <- c("#30123BFF", "#1AE4B6FF", "#FABA39FF", "#7A0403FF") # viridis::turbo(4)
 
@@ -18,3 +18,14 @@ temp_bin_ax_lab <- 'Air Temperature bin (°C)'
 ip_y_ax_lab <- 'Interception Efficiency (-)'
 temp_ax_lab <- 'Air Temperature (°C)'
 wind_ax_lab <- 'Wind Speed (m/s)'
+diam_ax_lab <- 'Hydrometeor Diameter (mm)'
+vel_ax_lab <- 'Hydrometeor Velocity (m/s)'
+
+label_bin_fn <- function(bins){
+  (bins[-1] + bins[-length(bins)]) / 2
+}
+
+give.n <- function(x){
+  return(c(y = median(x)*1.05, label = length(x)))
+  # experiment with the multiplier to find the perfect position
+}
