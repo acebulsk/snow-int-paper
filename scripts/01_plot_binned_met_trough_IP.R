906162977a2406b2fb7d652b5834ce77fe9ea883
Alex Cebulski <alexcebulski@gmail.com>
2024-01-05 16:51:20 -0700

    add margin space to plot boxplots all on one figure

--- scripts/01_plot_binned_met_trough_IP.R
+++ scripts/01_plot_binned_met_trough_IP.R
@@ -25,16 +25,17 @@ at_ip <- met_intercept |>
   # filter(n > 10) |>
   ggplot(aes(x = temp_labs, y = IP, group = temp_labs)) +
   # ggplot(aes(x = temp_labs, y = IP, group = temp_labs, fill = temp_labs)) +
-  geom_point(alpha=0.2, colour = "#56B4E9")+
+  # geom_point(alpha=0.2, colour = "#56B4E9")+
   geom_boxplot(width = 1)+
   # geom_point() +
   # geom_errorbar(aes(ymax = ci_hi, ymin = ci_low))  +
-  ylab("") +
+  ylab(ip_y_ax_lab) +
   xlab(temp_bin_ax_lab) +
   # scale_fill_viridis_c(option = 'magma')+
   xlim(NA, 0.5) +
   ylim(ip_y_lims) +
-  theme(legend.title = element_blank())
+  theme(legend.title = element_blank(),
+        plot.margin = margin(0.5, 0.5, 0.5, .75, "cm"))
 
 ws_ip <- met_intercept |>
   group_by(wind_labs) |>
@@ -46,12 +47,13 @@ ws_ip <- met_intercept |>
   mutate(n = n()) |>
   filter(n > 4) |>
   ggplot(aes(wind_labs, IP, group = wind_labs)) +
-  geom_point(alpha = 0.2, colour = "#56B4E9") +
+  # geom_point(alpha = 0.2, colour = "#56B4E9") +
   geom_boxplot()+
   # geom_errorbar(aes(ymax = ci_hi, ymin = ci_low))  +
   ylab(ip_y_ax_lab) +
   xlab(wnd_bin_ax_lab)+
-  ylim(ip_y_lims)
+  ylim(ip_y_lims)+
+  theme(plot.margin = margin(0.5, 0.5, 0.5, .75, "cm"))
 
 w_ip <- met_intercept |>
   group_by(tree_labs) |>
@@ -62,14 +64,15 @@ w_ip <- met_intercept |>
   mutate(n = n()) |>
   # filter(n > 10) |>
   ggplot(aes(tree_labs, IP, group = tree_labs)) +
-  geom_point(alpha = 0.5, colour = "#56B4E9") +
+  # geom_point(alpha = 0.5, colour = "#56B4E9") +
   # geom_errorbar(aes(ymax = ci_hi, ymin = ci_low))  +
   geom_boxplot() +
-  ylab("") +
+  ylab(ip_y_ax_lab) +
   xlab('Canopy Snow Load (mm)')+
-  ylim(ip_y_lims)
+  ylim(ip_y_lims)+
+  theme(plot.margin = margin(0.5, 0.5, 0.5, .75, "cm"))
 
 
-cowplot::plot_grid(at_ip, ws_ip, w_ip, nrow = 3)
+cowplot::plot_grid(at_ip, ws_ip, w_ip, nrow = 3, labels = 'AUTO')
 
 ggsave('figs/interception/troughs_met_vs_IP_bin.png', device = png, width = 8.5, height = 7)
