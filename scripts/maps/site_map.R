74e029b398540f1f9a543b2287098a7b2aecd309
Alex Cebulski <alexcebulski@gmail.com>
2024-06-17 13:46:42 -0600

    revised site map

--- scripts/maps/site_map.R
+++ scripts/maps/site_map.R
@@ -6,6 +6,7 @@ library(tidyterra)
 library(sf)
 library(tmap)
 
+# MAIN MAP ----
 plot_name_dict <- data.frame(
   name = c('PWL_E', 'FSR_S'),
   plot_name = c('PWL Plot', 'FT Plot')
@@ -58,9 +59,9 @@ bg <- terra::rast('/media/alex/phd-data/local-usask/analysis/lidar/gis/22_292FT_
 bg_resamp <- terra::rast('/media/alex/phd-data/local-usask/analysis/lidar/gis/22_292FT_RGB_resamp_25cm.tif')
 
 bbox <- st_bbox(lidr_flight_path)
-# bbox['xmin'] <- bbox['xmin'] - 50
+bbox['xmax'] <- bbox['xmax'] + 50
 # bbox['ymax'] <- bbox['ymax'] + 50
-tm <- tm_shape(bg_resamp, bbox = bbox) +
+main_map <- tm_shape(bg_resamp, bbox = bbox) +
   tm_rgb(colorNA = NULL)  +
   tm_graticules(
     ticks = TRUE,
@@ -97,6 +98,87 @@ tm <- tm_shape(bg_resamp, bbox = bbox) +
                 col = 'orange',
                 labels = c("In-situ Transect"))
 
-# tm
+# main_map
 
-tmap::tmap_save(tm, 'figs/maps/site_map.png', width = 6, height = 4, unit = 'in')
+tmap::tmap_save(main_map, 'figs/maps/site_map.png', width = 6, unit = 'in')
+
+# INSET MAP ----
+
+library(sf)
+library(tidyverse)
+library(tmap)
+library(terra)
+library(grid)
+
+data(land)
+
+land_ele <- terra::rast(land)[[4]]
+
+sites <- data.frame(
+  site = c('Wolf Creek', 'Russell Creek', 'Fortress Mountain', 'Marmot Creek'),
+  lon = c(-135.1497, -126.3090,-115.1983, -115.155154),
+  lat = c(60.567, 50.3710, 50.8269, 50.9255877)
+) |>
+  filter(site == 'Fortress Mountain')
+
+sites_sf <- sf::st_as_sf(sites, coords = c('lon', 'lat'), crs = st_crs(land))
+
+# sf::write_sf(sites_sf, 'data/gis/site_coords.gpkg')
+
+site_sf_buff <- st_buffer(sites_sf, dist = 2500000)
+bb <- st_bbox(site_sf_buff)
+
+canusa <-
+  spData::world %>%
+  dplyr::filter(name_long %in% c('Canada', 'United States', 'Mexico')) |>
+  st_transform(st_crs(land))
+
+land_crop <- terra::crop(land_ele, canusa)
+
+bb_canusa <- st_bbox(st_buffer(canusa , dist = 100))
+
+inset_map <-
+  # tm_shape(land_crop, bbox = bb) +
+  # tm_raster(
+  #   "elevation_elevation",
+  #   palette = terrain.colors(10),
+  #   breaks = c(0, 100, 300, 700, 1500, 2500, 4500),title = 'Elevation (m)'
+  # ) +
+  # tm_shape(canusa, bbox = bb) +
+  # tm_borders() +
+  tm_shape(canusa, bbox = bb) +
+  tm_polygons() +
+  tm_shape(sites_sf) +
+  tm_dots(col = 'red', size = 0.1) +
+  tm_text("site", size = 0.75, auto.placement = F, just = 'bottom', ymod = 0.1) +
+  # tm_graticules(n.x = 3, n.y = 4, ) +
+  tm_layout(legend.position = c('left', 'bottom'))
+inset_map
+tmap::tmap_save(inset_map, 'figs/maps/site_map_na_scale.png', width = 3)
+
+# put inset on main plot ----
+
+norm_dim = function(obj){
+  bbox = st_bbox(obj)
+  width = bbox[["xmax"]] - bbox[["xmin"]]
+  height = bbox[["ymax"]] - bbox[["ymin"]]
+  w = width / max(width, height)
+  h = height / max(width, height)
+  return(c(w, h))
+}
+
+main_dim = norm_dim(lidr_flight_path)
+ins_dim = norm_dim(site_sf_buff)
+
+main_vp <- viewport(width = main_dim[1], height = main_dim[2],default.units = 'snpc')
+ins_vp <- viewport(width = ins_dim[1] * 0.3, height = ins_dim[2] * 0.3,
+                  x = unit(15.5, "cm"), y = unit(4.5, "cm"))
+
+tmap::tmap_save(
+  main_map,
+  filename = 'figs/maps/site_map_inset.png',
+  width = 7,
+  height = 7,
+  insets_tm = inset_map,
+  insets_vp = ins_vp
+)
