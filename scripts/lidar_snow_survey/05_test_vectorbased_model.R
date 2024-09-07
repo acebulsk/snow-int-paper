ce204c944df8729962fce287cdbb67899cd4bf5e
Alex Cebulski <alexcebulski@gmail.com>
2024-09-06 18:09:24 -0600

    important !!! changed to not use alpha as we are using nadir CC so just need the offset

--- scripts/lidar_snow_survey/05_test_vectorbased_model.R
+++ scripts/lidar_snow_survey/05_test_vectorbased_model.R
@@ -1,6 +1,25 @@
 # Script to test model of shifted trajectory angle
 # Run 04 first for some of the objects below
 
+base_path <- '../../analysis/lidar-processing/data/dsm_ip/'
+pre_post_id <- '23_072_23_073'
+# pre_post_id <- '23_026_23_027' # if comparing to this one need to update the event totals for precip and wind, similar avg wind on this event from differing directions so may also have a wind effect
+
+vox_config_id <- '_v2.0.0_sa_'
+ip_config_id <- '_ip_normalised_resample_0.25_crop_mask'
+
+# need the interception efficiency obs to validate the model
+plot <- 'FSR_S'
+ft_ip_obs_rast <-
+  rast(paste0(base_path, pre_post_id, vox_config_id, plot, ip_config_id, '.tif'))
+
+plot <- 'PWL_E'
+pwl_ip_obs_rast <-
+  rast(paste0(base_path, pre_post_id, vox_config_id, plot, ip_config_id, '.tif'))
+
+# also validate the model using the observed lca for the given zenith angle range
+
+
 # Calculate the event trajectory angle ----
 
 paste0('Given the height of wind deemed important for PWL: ', pwl_wind_height_of_event |> round(2),
@@ -41,6 +60,10 @@ ggplot(event_wind_profile |>
     y = 'Height Above Snowpack (m)',
     x = 'Average Wind Speed (m/s)',
     # title = 'March 13-14, 2023 Wind Profile'
+  ) +
+  lims(
+    y = c(0, NA),
+    x = c(0, NA)
   ) +
   facet_wrap(~name, scales = 'free')
 
@@ -86,23 +109,23 @@ pwl_event_ta <- traj_angle_deg(pwl_wind, mean_vel)
 # where $u_2$ is the wind speed at a height of 2 m and $u_z$ is at the given
 # height $z$, and $z_0$ is the roughness height.
 
-adjust_wind <- function(wind_speed, z, z2, z_0m){
-  u2 <- wind_speed * (log(z2/z_0m) / log(z/z_0m))
-  return(u2)
-}
-
-ft_wind_estimate <- adjust_wind(event_avg_wind,
-                                z = us_wind_height,
-                                z2 = ft_wind_height_of_event,
-                                z_0m = ft_wp_pars$z_0m)
-
-ft_event_ta_estimate <- traj_angle_deg(ft_wind_estimate, mean_vel)
-
-pwl_wind_estimate <- adjust_wind(event_avg_wind,
-                                z = us_wind_height,
-                                z2 = pwl_wind_height_of_event,
-                                z_0m = ft_wp_pars$z_0m)
-pwl_event_ta_estimate <- traj_angle_deg(pwl_wind_estimate, mean_vel)
+# adjust_wind <- function(wind_speed, z, z2, z_0m){
+#   u2 <- wind_speed * (log(z2/z_0m) / log(z/z_0m))
+#   return(u2)
+# }
+#
+# ft_wind_estimate <- adjust_wind(event_avg_wind,
+#                                 z = us_wind_height,
+#                                 z2 = ft_wind_height_of_event,
+#                                 z_0m = ft_wp_pars$z_0m)
+#
+# ft_event_ta_estimate <- traj_angle_deg(ft_wind_estimate, mean_vel)
+#
+# pwl_wind_estimate <- adjust_wind(event_avg_wind,
+#                                 z = us_wind_height,
+#                                 z2 = pwl_wind_height_of_event,
+#                                 z_0m = ft_wp_pars$z_0m)
+# pwl_event_ta_estimate <- traj_angle_deg(pwl_wind_estimate, mean_vel)
 
 # so if we used the basic wind profile estimate above we would be over
 # estimating the traj angle by about 10 deg.
@@ -128,19 +151,19 @@ pwl_event_ta_estimate <- traj_angle_deg(pwl_wind_estimate, mean_vel)
 # because our measurements are inherently underestimates since we are missing a
 # lot of subcanopy snow depths
 
-mcn_df_smry <- readRDS(paste0(
-  '../../analysis/lidar-processing/data/hemi_stats/avg_voxrs_canopy_metrics_over_nadir_and_upper_hemi_2.5thpercentile_FT_PWL.rds'
-))
+pwl_cc_nadir_025 <- readRDS('../../analysis/lidar-processing/data/hemi_stats/pwl_lca_avg_event_theta_for_each_phi.rds') |>
+  as.data.frame() |>
+  filter(phi_d == 0)
 
-ft_cc_nadir <- mcn_df_smry |>
-  filter(plot_name == 'FT',
-         group == 'Nadir') |>
+pwl_cc_nadir <- pwl_cc_nadir_025 |>
   pull(lca) |>
   mean(na.rm = T)
 
-pwl_cc_nadir <- mcn_df_smry |>
-  filter(plot_name == 'PWL',
-         group == 'Nadir') |>
+ft_cc_nadir_025 <- readRDS('../../analysis/lidar-processing/data/hemi_stats/ft_lca_avg_event_theta_for_each_phi.rds') |>
+  as.data.frame() |>
+  filter(phi_d == 0)
+
+ft_cc_nadir <- ft_cc_nadir_025 |>
   pull(lca) |>
   mean(na.rm = T)
 
@@ -168,21 +191,28 @@ ft_lca <- ft_cc_nadir + ft_lca_inc
 
 pwl_lca <- pwl_cc_nadir + pwl_lca_inc
 
-# Calculate the interception efficiency for the event
-ip_model_coefs <- readRDS('../../analysis/lidar-processing/data/models/lca_vs_ip_model_error_ft_pwl_nadir_adj.rds')
+# Calculate the interception efficiency for the event Originally used an alpha
+# value based on the association between lca and ip however this only worked
+# when using earler iterations of the zenith angle choice, and since we are
+# using the nadir lca as a basis (which follows the 1:1 line in the original
+# plot just with a lot of scatter) we can see we are better off not using an
+# alpha value but just need to increase the lca by some factor select_ip_model
 
-ft_a <- ip_model_coefs |>
-  filter(`Plot Name` == 'FT',
-         `Canopy Metrics` == 'Vector Based') |>
-  pull(`Model Slope`)
+# <- 'Vector Based' select_ip_model <- 'Single Zenith Single Theta'
+# ip_model_coefs <-
+# readRDS('../../analysis/lidar-processing/data/models/lca_vs_ip_model_error_ft_pwl_nadir_adj.rds')
+#
+# ft_a <- ip_model_coefs |> filter(`Plot Name` == 'FT', `Canopy Metrics` ==
+# select_ip_model) |> pull(`Model Slope`)
+#
+# pwl_a <- ip_model_coefs |> filter(`Plot Name` == 'PWL', `Canopy Metrics` ==
+# select_ip_model) |> pull(`Model Slope`)
+#
+# ft_ip_mod <- ft_lca * ft_a
+# pwl_ip_mod <- pwl_lca * pwl_a
 
-pwl_a <- ip_model_coefs |>
-  filter(`Plot Name` == 'PWL',
-         `Canopy Metrics` == 'Vector Based') |>
-  pull(`Model Slope`)
-
-ft_ip_mod <- ft_lca * ft_a
-pwl_ip_mod <- pwl_lca * pwl_a
+ft_ip_mod <- ft_lca
+pwl_ip_mod <- pwl_lca
 
 # found these actually did pretty well but vector based still better
 # ft_ip_mod <- ft_cc_nadir # could agrue IP == nadir canopy coverage as in rainfall params
@@ -191,46 +221,48 @@ pwl_ip_mod <- pwl_lca * pwl_a
 # Error Analysis
 library(terra)
 
-ft_ip_obs <-
-  rast("../../analysis/lidar-processing/data/dsm_ip/23_072_23_073_v2.0.0_sa_FSR_S_ip_normalised_resample_0.25_crop_mask.tif") |>
+ft_ip_obs <- ft_ip_obs_rast |>
   values() |>
   mean(na.rm = T)
-pwl_ip_obs <-
-  rast("../../analysis/lidar-processing/data/dsm_ip/23_072_23_073_v2.0.0_sa_PWL_E_ip_normalised_resample_0.25_crop_mask.tif") |>
+pwl_ip_obs <- pwl_ip_obs_rast |>
   values() |>
   mean(na.rm = T)
 
 # Vector Based
 errors <- data.frame(
   ip = c(ft_ip_obs, pwl_ip_obs, ft_ip_mod, pwl_ip_mod,ft_cc_nadir, pwl_cc_nadir),
-  group = c('obs','obs', 'vb', 'vb', 'nadir', 'nadir'),
+  group = c('UAV-lidar','UAV-lidar', 'VB-model', 'VB-model', 'Nadir-model', 'Nadir-model'),
   plot = c('FT', 'PWL', 'FT', 'PWL', 'FT', 'PWL')) |>
   mutate(tf = (1-ip)*event_precip) |>
   pivot_longer(c(ip, tf)) |>
   pivot_wider(names_from = c(group), values_from = value) |>
-  pivot_longer(c(vb, nadir), names_to = 'mod_type', values_to = 'mod_vals') |>
-  mutate(bias = obs - mod_vals,
+  pivot_longer(c(`VB-model`, `Nadir-model`), names_to = 'mod_type', values_to = 'mod_vals') |>
+  mutate(bias = `UAV-lidar` - mod_vals,
          MAE = abs(bias),
-         `Perc. Error` = (bias)/obs*100)
+         `Perc. Error` = (bias)/`UAV-lidar`*100)
 errors
 
-saveRDS(errors, 'data/model_results/plot_scale_vb_model_error.rds')
+saveRDS(errors,
+        paste0(
+          'figs/lidar_periods/',
+          pre_post_id,
+          '_plot_scale_vb_model_error.rds'))
 
 # plot accumulated throughfall over the event for each model
 tf_df <- errors |>
   filter(name == 'tf') |>
   select(plot:mod_vals,-name) |>
   pivot_wider(names_from = mod_type, values_from = mod_vals) |>
-  pivot_longer(c(obs, vb, nadir))
+  pivot_longer(c(`UAV-lidar`, `VB-model`, `Nadir-model`))
 ggplot(tf_df, aes(plot, value, fill = name)) +
   geom_bar(stat = "identity", position = 'dodge') +
   labs(
-    y = '𝚫 Throughfall (kg m⁻²)',
+    y = '𝚫SWE (kg m⁻²)',
     x = element_blank(),
-    fill = 'Data Type'
+    fill = 'Event Throughfall'
   ) +
   ylim(c(0, 30)) +
-  geom_hline(aes(yintercept = event_precip, linetype = "𝚫 SWEo \n(kg m⁻²)"), color = "black", show.legend = TRUE) +
+  geom_hline(aes(yintercept = event_precip, linetype = "Event Snowfall"), color = "black", show.legend = TRUE) +
   scale_linetype_manual(values = "dashed", name = NULL) +
   guides(
     fill = guide_legend(override.aes = list(linetype = "blank")),
@@ -238,7 +270,10 @@ ggplot(tf_df, aes(plot, value, fill = name)) +
   )
 
 ggsave(
-  'figs/lidar_periods/20230314_event_throughfall_totals_obs_vs_vb_vs_nadir.png',
+  paste0(
+  'figs/lidar_periods/',
+  pre_post_id,
+  '_event_throughfall_totals_obs_vs_vb_vs_nadir.png'),
   width = 5,
   height = 3
 )
@@ -266,198 +301,34 @@ error_summary_noplots <- errors |>
 
 error_summary_noplots
 
-# test at the 5 m grid scale ----
-
-rsmpl_res <- 5
-agg_fn <- 'average'
-pwl_tf_025 <- rast("../../analysis/lidar-processing/data/dsm_swe/23_072_23_073_v2.0.0_sa_PWL_E_swe_normalised_resample_0.25_crop_mask.tif")
-pwl_tf_5m <- resample_rast(pwl_tf_025, rsmpl_res, agg_fn)
-names(pwl_tf_5m) <- 'tf_obs'
-ft_tf_025 <- rast("../../analysis/lidar-processing/data/dsm_swe/23_072_23_073_v2.0.0_sa_FSR_S_swe_normalised_resample_0.25_crop_mask.tif")
-ft_tf_5m <- resample_rast(ft_tf_025, rsmpl_res, agg_fn)
-names(ft_tf_5m) <- 'tf_obs'
-
-pwl_ip_5m <- rast("../../analysis/lidar-processing/data/dsm_ip/23_072_23_073_v2.0.0_sa_PWL_E_ip_normalised_resample_5m_crop_mask.tif")
-names(pwl_ip_5m) <- 'ip_obs'
-ft_ip_5m <- rast("../../analysis/lidar-processing/data/dsm_ip/23_072_23_073_v2.0.0_sa_FSR_S_ip_normalised_resample_5m_crop_mask.tif")
-names(ft_ip_5m) <- 'ip_obs'
-
-pwl_cc_nadir_5m <- rast('../../analysis/lidar-processing/data/dsm_cpy_metrics/23_072_23_073_v2.0.0_sa_PWL_E_nadir_lca_5m_crop_mask.tif')
-ft_cc_nadir_5m <- rast('../../analysis/lidar-processing/data/dsm_cpy_metrics/23_072_23_073_v2.0.0_sa_FSR_S_nadir_lca_5m_crop_mask.tif')
-
-# compute interception efficiency using the vector based model
-
-pwl_ip_vb_5m <- (pwl_cc_nadir_5m + pwl_lca_inc)*pwl_a
-names(pwl_ip_vb_5m) <- 'ip_mod'
-ft_ip_vb_5m <- (ft_cc_nadir_5m + ft_lca_inc)*ft_a
-names(ft_ip_vb_5m) <- 'ip_mod'
-
-pwl_cp_vb_5m <- (pwl_cc_nadir_5m + pwl_lca_inc)
-names(pwl_cp_vb_5m) <- 'C_p_mod'
-ft_cp_vb_5m <- (ft_cc_nadir_5m + ft_lca_inc)
-names(ft_cp_vb_5m) <- 'C_p_mod'
-
-# Leaf Contact Area ----
-
-pwl_cp_df <- c(pwl_ip_5m, pwl_cp_vb_5m) |>
-  terra::as.points() |>
-  as.data.frame(geom="XY") |>
-  # pivot_longer(c(ip_obs, ip_mod)) |>
-  mutate(plot_name = 'PWL')
-
-ft_cp_df <- c(ft_ip_5m, ft_cp_vb_5m) |>
-  terra::as.points() |>
-  as.data.frame(geom="XY") |>
-  # pivot_longer(c(ip_obs, ip_mod)) |>
-  mutate(plot_name = 'FT')
-
-cp_df_5m <- rbind(pwl_cp_df, ft_cp_df)
-
-ggplot(cp_df_5m, aes(C_p_mod, ip_obs)) +
-  geom_point() +
-  geom_abline(aes(slope = 1, intercept = 0, linetype = "1:1 line"), alpha = 0.5) +
-  facet_grid(~plot_name) +
-  lims(
-    y = c(0,NA),
-    x = c(0,NA)
-  )
-
-# IP ----
-
-## map 5m performance ----
-
-plot(ft_ip_5m - ft_ip_vb_5m)
-
-plot(pwl_ip_5m - pwl_ip_vb_5m)
-
-## plot 5 m performance with vector based adjustment ----
-
-pwl_ip_df <- c(pwl_ip_5m, pwl_ip_vb_5m) |>
-  terra::as.points() |>
-  as.data.frame(geom="XY") |>
-  # pivot_longer(c(ip_obs, ip_mod)) |>
-  mutate(plot_name = 'PWL')
-
-ft_ip_df <- c(ft_ip_5m, ft_ip_vb_5m) |>
-  terra::as.points() |>
-  as.data.frame(geom="XY") |>
-  # pivot_longer(c(ip_obs, ip_mod)) |>
-  mutate(plot_name = 'FT')
-
-ip_df_5m <- rbind(pwl_ip_df, ft_ip_df)
-
-ggplot(ip_df_5m, aes(ip_obs, ip_mod)) +
-  geom_point() +
-  geom_abline(aes(slope = 1, intercept = 0, linetype = "1:1 line"), alpha = 0.5) +
-  facet_grid(~plot_name)
-
-error_tbl_vb <- ip_df_5m |>
-  mutate(diff = ip_obs - ip_mod) |>
-  group_by(plot_name) |>
-  summarise(
-    `Mean Bias` = mean(diff, na.rm = T),
-    # `Max Error` = diff[which.max(abs(diff))],
-    MAE = mean(abs(diff), na.rm = T),
-    `RMS Error` = sqrt(mean(diff ^ 2, na.rm = T)))
-
-## plot 5 m performance using nadir cc as ip proxy ----
-
-pwl_ip_df <- c(pwl_ip_5m, pwl_cc_nadir_5m) |>
-  terra::as.points() |>
-  as.data.frame(geom="XY") |>
-  # pivot_longer(c(ip_obs, ip_mod)) |>
-  mutate(plot_name = 'PWL')
-
-ft_ip_df <- c(ft_ip_5m, ft_cc_nadir_5m) |>
-  terra::as.points() |>
-  as.data.frame(geom="XY") |>
-  # pivot_longer(c(ip_obs, ip_mod)) |>
-  mutate(plot_name = 'FT')
-
-ip_df_5m <- rbind(pwl_ip_df, ft_ip_df)
-
-ggplot(ip_df_5m, aes(ip_obs, Nadir)) +
-  geom_point() +
-  geom_abline(aes(slope = 1, intercept = 0, linetype = "1:1 line"), alpha = 0.5) +
-  facet_grid(~plot_name)
-
-error_tbl_cc_nadir <- ip_df_5m |>
-  mutate(diff = ip_obs - Nadir) |>
-  group_by(plot_name) |>
-  summarise(
-    `Mean Bias` = mean(diff, na.rm = T),
-    # `Max Error` = diff[which.max(abs(diff))],
-    MAE = mean(abs(diff), na.rm = T),
-    `RMS Error` = sqrt(mean(diff ^ 2, na.rm = T)))
-
-
-# Throughfall ----
-
-# calculate throughfall using vector based I/P
-
-pwl_tf_vb_5m <- (1-pwl_ip_vb_5m) * event_precip
-names(pwl_tf_vb_5m) <- 'tf_mod'
-ft_tf_vb_5m <- (1-ft_ip_vb_5m) * event_precip
-names(ft_tf_vb_5m) <- 'tf_mod'
-
-plot(ft_tf_5m - ft_tf_vb_5m)
-
-plot(pwl_tf_5m - pwl_tf_vb_5m)
-
-pwl_tf_df <- c(pwl_tf_5m, pwl_tf_vb_5m) |>
-  terra::as.points() |>
-  as.data.frame(geom="XY") |>
-  # pivot_longer(c(ip_obs, ip_mod)) |>
-  mutate(plot_name = 'PWL')
-
-ft_tf_df <- c(ft_tf_5m, ft_tf_vb_5m) |>
-  terra::as.points() |>
-  as.data.frame(geom="XY") |>
-  # pivot_longer(c(ip_obs, ip_mod)) |>
-  mutate(plot_name = 'FT')
-
-tf_df_5m <- rbind(pwl_tf_df, ft_tf_df)
-
-ggplot(tf_df_5m, aes(tf_obs, tf_mod)) +
-  geom_point() +
-  geom_abline(aes(slope = 1, intercept = 0, linetype = "1:1 line"), alpha = 0.5) +
-  facet_grid(~plot_name)
-
-error_tbl_tf <- tf_df_5m |>
-  mutate(diff = tf_obs - tf_mod) |>
-  group_by(plot_name) |>
-  summarise(
-    `Mean Bias` = mean(diff, na.rm = T),
-    # `Max Error` = diff[which.max(abs(diff))],
-    MAE = mean(abs(diff), na.rm = T),
-    `RMS Error` = sqrt(mean(diff ^ 2, na.rm = T)))
-
 # test at any grid scale ----
 
 # i've found that the error is pretty good around 5/10/20 m and gets better as
 # you get coarser/worse as you get finer. The differences between the two plots
 # in terms of the error also get smaller at coarser resolutions.
 
-rsmpl_res <- 20
-agg_fn <- 'average'
+rsmpl_res <- 5
+rsmpl_fact <- rsmpl_res/0.25
+agg_fn <- 'mean'
+min_n_cells <- 25
 pwl_tf_025 <- rast("../../analysis/lidar-processing/data/dsm_swe/23_072_23_073_v2.0.0_sa_PWL_E_swe_normalised_resample_0.25_crop_mask.tif")
-pwl_tf_rsmpl <- resample_rast(pwl_tf_025, rsmpl_res, agg_fn)
+pwl_tf_rsmpl <- resample_rast(pwl_tf_025, rsmpl_fact, agg_fn, min_n_cells)
 names(pwl_tf_rsmpl) <- 'tf_obs'
 ft_tf_025 <- rast("../../analysis/lidar-processing/data/dsm_swe/23_072_23_073_v2.0.0_sa_FSR_S_swe_normalised_resample_0.25_crop_mask.tif")
-ft_tf_rsmpl <- resample_rast(ft_tf_025, rsmpl_res, agg_fn)
+ft_tf_rsmpl <- resample_rast(ft_tf_025, rsmpl_fact, agg_fn, min_n_cells)
 names(ft_tf_rsmpl) <- 'tf_obs'
 
 pwl_ip_025 <- rast("../../analysis/lidar-processing/data/dsm_ip/23_072_23_073_v2.0.0_sa_PWL_E_ip_normalised_resample_0.25_crop_mask.tif")
-pwl_ip_rsmpl <- resample_rast(pwl_ip_025, rsmpl_res, agg_fn)
+pwl_ip_rsmpl <- resample_rast(pwl_ip_025, rsmpl_fact, agg_fn, min_n_cells)
 names(pwl_ip_rsmpl) <- 'ip_obs'
 ft_ip_025 <- rast("../../analysis/lidar-processing/data/dsm_ip/23_072_23_073_v2.0.0_sa_FSR_S_ip_normalised_resample_0.25_crop_mask.tif")
-ft_ip_rsmpl <- resample_rast(ft_ip_025, rsmpl_res, agg_fn)
+ft_ip_rsmpl <- resample_rast(ft_ip_025, rsmpl_fact, agg_fn, min_n_cells)
 names(ft_ip_rsmpl) <- 'ip_obs'
 
-pwl_cc_nadir_025m <- rast('../../analysis/lidar-processing/data/dsm_cpy_metrics/23_072_23_073_v2.0.0_sa_PWL_E_nadir_lca_0.25m_crop_mask.tif')
-pwl_cc_nadir_rsmpl <- resample_rast(pwl_cc_nadir_025m, rsmpl_res, agg_fn)
-ft_cc_nadir_025m <- rast('../../analysis/lidar-processing/data/dsm_cpy_metrics/23_072_23_073_v2.0.0_sa_FSR_S_nadir_lca_0.25m_crop_mask.tif')
-ft_cc_nadir_rsmpl <- resample_rast(ft_cc_nadir_025m, rsmpl_res, agg_fn)
+pwl_cc_nadir_025m <- rast('../../analysis/lidar-processing/data/dsm_cpy_metrics/23_072_vox_len_0.25m_sa_gridgen_v2.0.0_sa_PWL_E_nadir_lca_0.25m_crop_mask.tif')
+pwl_cc_nadir_rsmpl <- resample_rast(pwl_cc_nadir_025m, rsmpl_fact, agg_fn, min_n_cells)
+ft_cc_nadir_025m <- rast('../../analysis/lidar-processing/data/dsm_cpy_metrics/23_072_vox_len_0.25m_sa_gridgen_v2.0.0_sa_FSR_S_nadir_lca_0.25m_crop_mask.tif')
+ft_cc_nadir_rsmpl <- resample_rast(ft_cc_nadir_025m, rsmpl_fact, agg_fn, min_n_cells)
 
 # compute interception efficiency using the vector based model
 
@@ -468,7 +339,7 @@ names(ft_ip_vb_rsmpl) <- 'ip_mod'
 
 # IP ----
 
-## map 5m performance ----
+## map Xm performance ----
 
 plot(ft_ip_rsmpl - ft_ip_vb_rsmpl)
 
@@ -503,7 +374,8 @@ error_tbl_vb <- ip_df_rsmpl |>
     `Mean Bias` = mean(diff, na.rm = T),
     # `Max Error` = diff[which.max(abs(diff))],
     MAE = mean(abs(diff), na.rm = T),
-    `RMS Error` = sqrt(mean(diff ^ 2, na.rm = T)))
+    `RMS Error` = sqrt(mean(diff ^ 2, na.rm = T)),
+    group = 'Vector Based')
 
 ## plot resampled performance using nadir cc as ip proxy ----
 
@@ -521,20 +393,23 @@ ft_ip_df <- c(ft_ip_rsmpl, ft_cc_nadir_rsmpl) |>
 
 ip_df_rsmpl <- rbind(pwl_ip_df, ft_ip_df)
 
-ggplot(ip_df_rsmpl, aes(ip_obs, Nadir)) +
+ggplot(ip_df_rsmpl, aes(ip_obs, lca)) +
   geom_point() +
   geom_abline(aes(slope = 1, intercept = 0, linetype = "1:1 line"), alpha = 0.5) +
   facet_grid(~plot_name)
 
 error_tbl_cc_nadir <- ip_df_rsmpl |>
-  mutate(diff = ip_obs - Nadir) |>
+  mutate(diff = ip_obs - lca) |>
   group_by(plot_name) |>
   summarise(
     `Mean Bias` = mean(diff, na.rm = T),
     # `Max Error` = diff[which.max(abs(diff))],
     MAE = mean(abs(diff), na.rm = T),
-    `RMS Error` = sqrt(mean(diff ^ 2, na.rm = T)))
+    `RMS Error` = sqrt(mean(diff ^ 2, na.rm = T)),
+    group = 'Nadir')
 
+error_tb_ip <- rbind(error_tbl_vb, error_tbl_cc_nadir)
+error_tb_ip
 
 # Throughfall ----
 
@@ -568,12 +443,56 @@ ggplot(tf_df_rsmpl, aes(tf_obs, tf_mod)) +
   geom_abline(aes(slope = 1, intercept = 0, linetype = "1:1 line"), alpha = 0.5) +
   facet_grid(~plot_name)
 
-error_tbl_tf <- tf_df_rsmpl |>
+error_tbl_tf_vb <- tf_df_rsmpl |>
   mutate(diff = tf_obs - tf_mod) |>
   group_by(plot_name) |>
   summarise(
     `Mean Bias` = mean(diff, na.rm = T),
     # `Max Error` = diff[which.max(abs(diff))],
     MAE = mean(abs(diff), na.rm = T),
-    `RMS Error` = sqrt(mean(diff ^ 2, na.rm = T)))
+    `RMS Error` = sqrt(mean(diff ^ 2, na.rm = T)),
+    group = 'Vector Based')
 
+# calculate throughfall using cc nadir as proxy for I/P
+
+pwl_tf_vb_rsmpl <- (1-pwl_cc_nadir_rsmpl) * event_precip
+names(pwl_tf_vb_rsmpl) <- 'tf_mod'
+ft_tf_vb_rsmpl <- (1-ft_cc_nadir_rsmpl) * event_precip
+names(ft_tf_vb_rsmpl) <- 'tf_mod'
+
+plot(ft_tf_rsmpl - ft_tf_vb_rsmpl)
+
+plot(pwl_tf_rsmpl - pwl_tf_vb_rsmpl)
+
+pwl_tf_df <- c(pwl_tf_rsmpl, pwl_tf_vb_rsmpl) |>
+  terra::as.points() |>
+  as.data.frame(geom="XY") |>
+  # pivot_longer(c(ip_obs, ip_mod)) |>
+  mutate(plot_name = 'PWL')
+
+ft_tf_df <- c(ft_tf_rsmpl, ft_tf_vb_rsmpl) |>
+  terra::as.points() |>
+  as.data.frame(geom="XY") |>
+  # pivot_longer(c(ip_obs, ip_mod)) |>
+  mutate(plot_name = 'FT')
+
+tf_df_rsmpl <- rbind(pwl_tf_df, ft_tf_df)
+
+ggplot(tf_df_rsmpl, aes(tf_obs, tf_mod)) +
+  geom_point() +
+  geom_abline(aes(slope = 1, intercept = 0, linetype = "1:1 line"), alpha = 0.5) +
+  facet_grid(~plot_name)
+
+error_tbl_tf_nadir <- tf_df_rsmpl |>
+  mutate(diff = tf_obs - tf_mod) |>
+  group_by(plot_name) |>
+  summarise(
+    `Mean Bias` = mean(diff, na.rm = T),
+    # `Max Error` = diff[which.max(abs(diff))],
+    MAE = mean(abs(diff), na.rm = T),
+    `RMS Error` = sqrt(mean(diff ^ 2, na.rm = T)),
+    group = 'Nadir')
+
+tf_error <- rbind(error_tbl_tf_vb,
+                  error_tbl_tf_nadir)
+tf_error
